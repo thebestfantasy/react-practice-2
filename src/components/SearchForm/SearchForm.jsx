b899db39074af902c692e4df6355df0082690140
thebestfantasy <sergej.fronch@gmail.com>
2023-09-23 15:19:35 +0300

    add gallery + searchForm

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,39 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  onInputChange = e => {
+    this.setState({
+      searchQuery: e.currentTarget.value.toLowerCase().trim(),
+    });
+  };
+
+  onHandleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.searchQuery);
+    this.setState({
+      searchQuery: '',
+    });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.onHandleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+          onChange={this.onInputChange}
+          value={this.state.searchQuery}
+        />
+      </SearchFormStyled>
+    );
   }
 }
