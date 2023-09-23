df2a661af2fbd4cbed47441d2fd2d017e7d52ca5
YarynaLe <109382584+YarynaLe@users.noreply.github.com>
2023-09-23 16:16:13 +0300

    +

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -10,10 +10,8 @@ export class Gallery extends Component {
     images: [],
     isLoading: false,
     error: null,
-  };
-
-  componentDidMount = () => {
-    this.getPhotos('cat', 1);
+    isEmpty: false,
+    isVisible: false,
   };
 
   componentDidUpdate = (_, prevState) => {
@@ -27,8 +25,20 @@ export class Gallery extends Component {
     if (!query) return;
     this.setState({ isLoading: true });
     try {
-      const data = await ImageService.getImages(query, page);
-      console.log(data);
+      const {
+        photos,
+        total_results,
+        per_page,
+        page: curentPage,
+      } = await ImageService.getImages(query, page);
+      if (photos.length === 0) {
+        this.setState({ isEmpty: true });
+      }
+
+      this.setState(prevState => ({
+        images: [...prevState.images, ...photos],
+        isVisible: curentPage < Math.ceil(total_results / per_page),
+      }));
     } catch (error) {
       this.setState({ error: error.message });
     } finally {
@@ -41,10 +51,27 @@ export class Gallery extends Component {
   };
 
   render() {
+    const { images, isLoading, isEmpty, isVisible, error } = this.state;
     return (
       <>
         <SearchForm onSubmit={this.handleSubmit} />
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        {isEmpty && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
+        {error && (
+          <Text textAlign="center">❌ Something went wrong - {error}</Text>
+        )}
+        {images.length > 0 && (
+          <Grid>
+            {images.map(({ id, avg_color, src, alt }) => (
+              <GridItem key={id}>
+                <CardItem color={avg_color}>
+                  <img src={src.large} alt={alt} />
+                </CardItem>
+              </GridItem>
+            ))}
+          </Grid>
+        )}
       </>
     );
   }
